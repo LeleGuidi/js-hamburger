02e590843742cbf3cd582bb096394ef26e940628
LeleGuidi <emanuele.guidi98@outlook.it>
2022-04-29 16:01:36 +0200

    Aggiunto ID al div hamburger-menu

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //Creare variabili per richiamare gli elementi interessati (icona menu, nav e icona X)
 const openMenu = document.getElementById(`open-menu`);
-const menu = document.getElementById(`hamburger-menu`);
+const menu = document.getElementById(`hamburger_menu`);
 const closeMenu = document.getElementById(`close-menu`);
-console.log(`openMenu`);
-console.log(`menu`);
-console.log(`closeMenu`);
+console.log(openMenu);
+console.log(menu);
+console.log(closeMenu);
 
 //Al cliccare sull'icona del menù, verrà visualizzata la nav
 openMenu.addEventListener(`click`,
