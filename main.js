8b1f62c8f662633b14c9c63edb431c8175c40d60
LeleGuidi <emanuele.guidi98@outlook.it>
2022-04-29 15:52:45 +0200

    Inserite variabili e funzioni al click dell'utente

---
+++ main.js
@@ -0,0 +1,21 @@
+//Creare variabili per richiamare gli elementi interessati (icona menu, nav e icona X)
+const openMenu = document.getElementById(`open-menu`);
+const menu = document.getElementById(`hamburger-menu`);
+const closeMenu = document.getElementById(`close-menu`);
+console.log(`openMenu`);
+console.log(`menu`);
+console.log(`closeMenu`);
+
+//Al cliccare sull'icona del menù, verrà visualizzata la nav
+openMenu.addEventListener(`click`,
+    function() {
+        menu.classList.add(`active`);
+    }
+);
+
+// Al cliccare sull'icona della X, la nav scompare
+closeMenu.addEventListener(`click`,
+    function() {
+        menu.classList.remove(`active`)
+    }
+);
